eceb46bbbbae704934c7b529b20d86a9be5929c3
Rohan Garg <rohgarg@ccs.neu.edu>
2018-05-04 12:59:18 -0400

    hash.h: Compute size of unsigned long at runtime

--- src/hash.h
+++ src/hash.h
@@ -34,40 +34,35 @@
  * machines where multiplications are slow.
  */
 
-#define BITS_PER_LONG	32 
-#if BITS_PER_LONG == 32
-/* 2^31 + 2^29 - 2^25 + 2^22 - 2^19 - 2^16 + 1 */
-#define GOLDEN_RATIO_PRIME 0x9e370001UL
-#elif BITS_PER_LONG == 64
-/*  2^63 + 2^61 - 2^57 + 2^54 - 2^51 - 2^18 + 1 */
-#define GOLDEN_RATIO_PRIME 0x9e37fffffffc0001UL
-#else
-#error Define GOLDEN_RATIO_PRIME for your wordsize.
-#endif
-
 static inline unsigned long hash_long(unsigned long val, unsigned int bits)
 {
 	unsigned long hash = val;
+  const unsigned BITS_PER_BYTE = 8;
+  const unsigned BITS_PER_LONG = sizeof(unsigned long) * BITS_PER_BYTE;
 
-#if BITS_PER_LONG == 64
-	/*  Sigh, gcc can't optimise this alone like it does for 32 bits. */
-	unsigned long n = hash;
-	n <<= 18;
-	hash -= n;
-	n <<= 33;
-	hash -= n;
-	n <<= 3;
-	hash += n;
-	n <<= 3;
-	hash -= n;
-	n <<= 4;
-	hash += n;
-	n <<= 2;
-	hash += n;
-#else
-	/* On some cpus multiply is faster, on others gcc will do shifts */
-	hash *= GOLDEN_RATIO_PRIME;
-#endif
+  if (BITS_PER_LONG == 64) {
+	 /*  Sigh, gcc can't optimise this alone like it does for 32 bits. */
+	 unsigned long n = hash;
+   /*  2^63 + 2^61 - 2^57 + 2^54 - 2^51 - 2^18 + 1 */
+   const unsigned long GOLDEN_RATIO_PRIME = 0x9e37fffffffc0001UL;
+	 n <<= 18;
+	 hash -= n;
+	 n <<= 33;
+	 hash -= n;
+	 n <<= 3;
+	 hash += n;
+	 n <<= 3;
+	 hash -= n;
+	 n <<= 4;
+	 hash += n;
+	 n <<= 2;
+	 hash += n;
+  } else {
+	 /* On some cpus multiply is faster, on others gcc will do shifts */
+   /* 2^31 + 2^29 - 2^25 + 2^22 - 2^19 - 2^16 + 1 */
+   const unsigned long GOLDEN_RATIO_PRIME = 0x9e370001UL;
+	 hash *= GOLDEN_RATIO_PRIME;
+  }
 
 	/* High bits are more random, so use them. */
 	return hash >> (BITS_PER_LONG - bits);
